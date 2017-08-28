10c5cb6ee8c8a8707694f7f836105a6e86b2183e
Scott Young <young.scottw@gmail.com>
2017-08-28 13:12:17 -0500

    Handle restriction of player ship movement outside of gamespace

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	private float padding = 0.5f;
+	private float xMin;
+	private float xMax;
+
+	void Start() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		xMin = leftmost.x + padding;
+		xMax = rightmost.x - padding;
+	}
 
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 		else if(Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		// Restrict the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
