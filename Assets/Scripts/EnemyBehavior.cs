db952aa2f1249a7417149c93f091857374f70a18
Scott Young <young.scottw@gmail.com>
2017-08-30 16:38:30 -0500

    Handle laser hitting enemies

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+	private float health = 150.0f;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile) {
+			missile.Hit();
+			health -= missile.GetDamage();
+			if(health <= 0) {
+				Destroy(gameObject);
+			}
+			Debug.Log("Hit by a projectile");
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage = 100.0f;
+	
+	public float GetDamage() {
+		return damage;
+	}
+
+	public void Hit() {
+		Destroy(gameObject);
+	}
+}
